580186fc3134ed749b0090b5fffb8095fe92f279
Claire Dennehy <claire.dennehy@dell.com>
2017-05-10 16:30:30 +0100

    Updated copyright

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Dell Project Symphony'
-copyright = '2017 Dell Inc. or its subsidiaries. All Rights Reserved.'
+copyright = '2017 Dell Inc. or its subsidiaries. All Rights Reserved'
 author = ' '
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
